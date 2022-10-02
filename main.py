8f6b7eb5f0154f2ee27ff0671aa6b4e4db9bd205
Thomas <taongaws07@gmail.com>
2022-10-02 19:08:36 +0100

    fixed file structure

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
-from ursina.prefabs.dropdown_menu import DropdownMenu, DropdownMenuButton
-from ursina import Ursina, Entity, Sky, EditorCamera, Slider
+from ursina import Ursina, Entity, EditorCamera, Text
+from math import pi
 
-app = Ursina()
+SUN_RADIUS = 6.957 * (10**8)
+SUN_MASS = 1.989 * 10**30
+SUN_LUM = 3.828 * 10**26
+SB = 5.670367 * 10**-8
+SUN_LIFE = 10**10
+SUN_TEMP = 5778
 
+length_held = 0 
+old_pos = (0,0)
 
 class Star(Entity):
-     def __init__(self, **kwargs):
-        super().__init__(model = "sphere", texture = 'sun_texture.jpg', scale=Slider())
+    def __init__(self, radius, temperture) -> None:
+        super().__init__(model = "sphere", texture = './recources/8k_sun.jpg', scale = 100)
+        self.radius, self.temperture = radius, temperture
+        self.lifetime = SUN_LIFE * ((8.852 * (10**20) * (radius **0.571) * ((temperture) ** 1.142)) / SUN_MASS) ** 2.5
+        self.luminenecence = lambda: SB * (4 * pi * (self.radius**2)) * (self.temperture**4)
+        self.mass = lambda: (self.lifetime / SUN_LIFE)**(1/2.5) * SUN_MASS
+        
+    def update(self) -> None:
+        self.radius = (((self.lifetime / SUN_LIFE) ** (1/2.5) * SUN_MASS) / (8.852 * 10**20 * (self.temperture ** 1.142)) ) ** (1/0.571)
+        self.temperture = (((self.lifetime / SUN_LIFE) ** (1/2.5) * SUN_MASS) / (8.852 * 10**20 * (self.radius ** 0.571)) ) ** (1/1.142)
+        colours=["#0000FF","#dbe9f4","#FFFFFF","#ffffd4","#FFFF00","#FFA500","#FF0000"]
+        if self.temperture >= 30000: self.color =  colours[0]
+        if 30000 > self.temperture >= 20000: self.color =  colours[1]
+        if 20000 > self.temperture >= 10000: self.color =  colours[2]
+        if 10000 > self.temperture >= 7000: self.color =  colours[3]
+        if 7000 > self.temperture >= 6000: self.color =  colours[4]
+        if 5000 > self.temperture >= 3000: self.color =  colours[5]
 
-cam = EditorCamera()
-cam.target_z -= cam.zoom_speed * (abs(cam.target_z)*20)
-
-
-earth = Star()
-Sky(color="#000000")
-
+def update():
+    a.lifetime -= 1
+    a.update_radius()
+    a.update_temp()
+    a.color = a.set_colour()
+    print(a.lifetime, a.temperture)
+# if __name__ == '__main__':
+app = Ursina()
+a = Star((6.957 * (10**8)), 5778)
+camera = EditorCamera()
 app.run()
--- test.py
+++
@@ -1,8 +0,0 @@
-import math
-from datetime import datetime
-
-
-jdt = lambda date:  367 * date.year - int((7 * (date.year + int((date.month + 9) / 12.0))) / 4.0) + int((275 * date.month) / 9.0) + date.day + 1721013.5 + (date.hour + date.minute / 60.0 + date.second / math.pow(60,2)) / 24.0 - 0.5 * math.copysign(1, 100 * date.year + date.month - 190002.5) + 0.5
-
-while True:
-    print(datetime.utcnow(), jdt(datetime.utcnow()))
